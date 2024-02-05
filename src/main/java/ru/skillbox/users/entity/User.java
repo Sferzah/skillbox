75d6e5eaf032de5c490474a9d696b010266ca9d0
sferz <sferzah@gmail.com>
2024-02-05 12:08:56 +0300

    hometask 1.13 liquibase

--- src/main/java/ru/skillbox/users/entity/User.java
+++ src/main/java/ru/skillbox/users/entity/User.java
@@ -19,13 +19,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "parent_name")
+    @Column(name = "middle_name")
     private String middleName;
 
-    @Column(name = "family")
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "gender")
